27a481ba5d1e0269f49e0ddc266c24dca27620c9
Polarbear08 <pistolstar0808@gmail.com>
2020-11-13 00:20:18 +0900

    add delegation

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,13 +1,20 @@
-pipiline {
-    agent {
-        label 'master'
-    }
-    options {
-        timestamps()
-    }
-    stages {
-        stage('test') {
-            sh 'echo hello world'
+def call(Closure body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipiline {
+        agent {
+            label 'master'
+        }
+        options {
+            timestamps()
+        }
+        stages {
+            stage('test') {
+                sh 'echo hello world'
+            }
         }
     }
 }
