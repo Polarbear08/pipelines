e1b36edebfb9c0b06d0f319afe8ddcfd91a0c881
Polarbear08 <pistolstar0808@gmail.com>
2020-11-15 23:59:58 +0900

    hi

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -3,9 +3,9 @@ def sayHello(Map args) {
     echo "helo ${params.message}, ${params.message2}"
 }
 
-def sayYeah(Map args) [
+def sayYeah(Map args) {
     def params = [yeah: 'Yeah!'] + (args ?: [:])
     sh """
         echo ${params.yeah}
     """
-]
+}
