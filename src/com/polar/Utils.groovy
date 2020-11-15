24c20b943d696340828f0c25ef5d079ecd572c5d
Polarbear08 <pistolstar0808@gmail.com>
2020-11-15 22:21:58 +0900

    call utilitiescall utilities

--- src/com/polar/Utils.groovy
+++ src/com/polar/Utils.groovy
@@ -10,8 +10,8 @@ class Utils implements Serializable {
     def sayHello(String message) {
         def printThis = this
         this.steps.sh "this: ${printThis.class.name}"
-        this.steps.sh "this.steps: ${this.steps.class.name}"
-        this.steps.sh "this.steps.sh: ${this.steps.sh.class.name}"
+        this.steps.sh "this.steps: ${printThis.steps.class.name}"
+        this.steps.sh "this.steps.sh: ${printThis.steps.sh.class.name}"
         this.steps.sh "echo hello, ${message}"
     }
 }
