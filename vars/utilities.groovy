b2aa02161d46e846e28123f76540585b8de015b6
Polarbear08 <pistolstar0808@gmail.com>
2020-11-15 23:55:07 +0900

    aba

--- vars/utilities.groovy
+++ vars/utilities.groovy
@@ -1,3 +1,4 @@
-def sayHello(Map params = [message: 'aa', message2: 'bb']) {
+def sayHello(Map args) {
+    def params = [message: 'aa', message2: 'bb'] + (args ?: [:])
     echo "helo ${params.message}, ${params.message2}"
 }
